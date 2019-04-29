137a629670681cc19c0cb5843980aa4b19acfb90
Tezz15 <tejnarayan15.tnc@outlook.com>
2019-04-29 08:53:12 +0545

    Some more java files

---
+++ strings.java
@@ -0,0 +1,15 @@
+public class strings {
+    public static void main(String[] args) {
+        String text = "hello";
+
+        String blank = " ";
+
+        String name = "Tej";
+
+        String greeting = text + blank + name;
+
+        System.out.println(greeting);  //method of printing string 
+
+        System.out.println("hello" + " " + "Tej"); // another cumbersome method of printing string
+    }
+}
---
+++ variables.java
@@ -0,0 +1,26 @@
+import java.io.*;
+public class variables {
+
+    public static void main(String[] args) {
+        int myNumber = 15;
+        long myLong = 98554555;
+        short myShort = 88;
+
+        double myDouble = 5.33333;
+        float myFloat = 233.3f;
+
+        char myChar = 'f';
+        boolean myBoolean = false;
+
+        byte myByte = 123;
+
+        System.out.println(myNumber);
+        System.out.println(myLong);
+        System.out.println(myShort);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+    }
+}
